7a856e110d85da3fa7aa2a4a2e713b3b8e1b816d
付晨伟 <wb-fcw525340@cainiao.com>
2019-11-04 11:47:46 +0800

    feat: useAsyncTask add

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,38 @@
-import React, {Component} from 'react'
+import React, { Component, useState} from 'react'
 import {render} from 'react-dom'
+import { Button, Spin } from 'antd';
+import 'antd/dist/antd.css';
+import { useAsyncTask } from '../../src'
 
-import Example from '../../src'
-
-class Demo extends Component {
-  render() {
-    return <div>
-      <h1>dany-react-hook-frame Demo</h1>
-      <Example/>
-    </div>
+const Demo = () => {
+  const [id, setId] = useState(1);
+  
+  function addID() {
+    setId(id + 1);
   }
+
+  function reduceId() {
+    setId(id - 1);
+  }
+  async function testFunction() {
+    const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    if (result.status !== 200) {
+      throw new Error('bad status = ' + result.status);
+    }
+    return result.json();
+  }
+
+  const moduleStateTest = useAsyncTask(testFunction, [id]);
+
+  return <div>
+    <Spin spinning={moduleStateTest.loading}>
+      <p>ID:{id}</p>
+      <Button type="primary" onClick={() => addID()}>add id</Button>
+      <Button type="danger" onClick={() => reduceId()}>reduce id</Button>
+      <Button>edit data</Button>
+    </Spin>
+    
+  </div>
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo/>, document.querySelector('#demo'));
---
+++ src/useAsyncTask.js
@@ -0,0 +1,40 @@
+import { useEffect, useState, useCallback, useMemo } from 'react';
+
+export const useAsyncTask = (asyncFunc, params) => {
+
+    const [reloadNum, setReloadNum] = useState(0);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const task = useCallback(async () => {
+        setLoading(true);
+        try {
+            let result = await asyncFunc();
+            setData(result);
+            setLoading(false);
+
+        } catch (e) {
+            setError(e);
+            setLoading(false);
+        }
+    }, [...params]);
+
+    function triggleReload() {
+        setReloadNum(reloadNum + 1);
+    }
+
+    useEffect(() => {
+        task();
+    }, [task, reloadNum, ...params]);
+
+    return useMemo(() => ({
+        error: error,
+        loading: loading,
+        data: data,
+        setData: (arg) => { setData(arg) },
+        reload: () => { triggleReload() }
+
+    }), [error, loading, data, setData, triggleReload]);
+
+}
